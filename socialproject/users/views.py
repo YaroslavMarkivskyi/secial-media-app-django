e821e1203c3db3bd21cc10726a9a1d422aca48ee
Yaroslav Markivskyi <yaroslav.markivskiy.it.2022@lpnu.ua>
2024-07-17 21:39:55 +0300

    feat: Implement user login functionality with form validation

--- socialproject/users/views.py
+++ socialproject/users/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth import authenticate, login
+
 from .forms import LoginForm
 # Create your views here.
 
 
 def user_login(request):
-    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.changed_data
+            user = authenticate(request,
+                         username=data['username'],
+                         password=data['password'])
+            if user is not None:
+                login(request, user)
+                return HttpResponse("user authenticated")
+            else:
+                return HttpResponse("user doesn't authenticated")
+
+    else:
+        form = LoginForm()
     return(request, 'users/login.html', {'form': form})
