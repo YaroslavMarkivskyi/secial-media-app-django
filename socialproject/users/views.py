dae11cdd9c2d454195712d74a614eb9b7c2977b7
Yaroslav Markivskyi <yaroslav.markivskiy.it.2022@lpnu.ua>
2024-07-17 21:30:16 +0300

    feat: Add user login functionality

--- socialproject/users/views.py
+++ socialproject/users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .forms import LoginForm
 # Create your views here.
+
+
+def user_login(request):
+    form = LoginForm()
+    return(request, 'users/login.html', {'form': form})
