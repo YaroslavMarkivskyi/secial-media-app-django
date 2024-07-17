96584fa17f83c71e3a8e5b13c42bdb658b5e85ef
Yaroslav Markivskyi <yaroslav.markivskiy.it.2022@lpnu.ua>
2024-07-17 21:46:28 +0300

    chore: Update logout URL in users app urlpatterns

--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
-    path('logout/', auth_view.LogoutView.as_view()),
+    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
