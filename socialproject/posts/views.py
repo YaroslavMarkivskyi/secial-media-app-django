4108935eb8be3f32e58034a7d1ac626ab2e4fb3b
Yaroslav Markivskyi <yaroslav.markivskiy.it.2022@lpnu.ua>
2024-07-18 21:33:49 +0300

    feat: Add lazy loading for images in page load
    This commit adds lazy loading functionality for images in the page load. By implementing lazy loading, the images will only load when they are visible in the viewport, improving the overall page load performance. This enhancement provides a better user experience by reducing the initial load time and optimizing the rendering of images.

--- socialproject/posts/views.py
+++ socialproject/posts/views.py
@@ -7,7 +7,7 @@ from .forms import PostCreateForm
 @login_required
 def post_create(request):
     if request.method == 'POST':
-        form = PostCreateForm(data=request.POST)
+        form = PostCreateForm(data=request.POST, files=request.FILES)
         if form.is_valid():
             new_item = form.save(commit=False)
             new_item.user = request.user
