1959ce91e647265c810d58611755f85b0245f39d
Yaroslav Markivskyi <yaroslav.markivskiy.it.2022@lpnu.ua>
2024-07-18 12:34:11 +0300

    chore: Add password reset confirm and complete URLs to users app urlpatterns

--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -18,5 +18,9 @@ urlpatterns = [
          name='password_reset'),
     path('password_reset/done',
          auth_view.PasswordChangeDoneView.as_view(template_name='users/password_reset_done.html'),
-         name='password_reset_done')
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_view.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset/done/', auth_view.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+         name='password_reset_complete'),
     ]
