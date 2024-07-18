a9d811429ef656af658914e8236ea9314fc6b0cd
Yaroslav Markivskyi <yaroslav.markivskiy.it.2022@lpnu.ua>
2024-07-18 22:31:49 +0300

    feat: Implement like functionality for posts
    This commit adds the ability for users to like and unlike posts. It introduces a new view function `like_post` that handles the logic for toggling the like status of a post. When a user likes a post, their user ID is added to the `liked_by` field of the post. If the user has already liked the post, their ID is removed from the `liked_by` field. This feature enhances user engagement and allows users to show their appreciation for posts they enjoy.

--- socialproject/posts/views.py
+++ socialproject/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 
@@ -34,3 +34,13 @@ def feed(request):
     posts = Post.objects.all()
     logged_user = request.user
     return render(request, 'posts/feed.html', {'posts': posts, 'logged_user': logged_user, 'comment_form': comment_form})
+
+
+def like_post(request):
+    post_id = request.POST.get('post_id')
+    post = get_object_or_404(Post, id=post_id)
+    if post.liked_by.filter(id=request.user.id).exists():
+        post.liked_by.remove(request.user)
+    else:
+        post.liked_by.add(request.user)
+    return redirect('feed')
