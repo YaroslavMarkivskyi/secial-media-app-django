e25499d8886606b8aec2a46a99054ff42e07dc21
Yaroslav Markivskyi <yaroslav.markivskiy.it.2022@lpnu.ua>
2024-07-18 21:16:26 +0300

    ```text refactor: Add slug generation to Post model's save method
    The code changes in this commit add a slug generation functionality to the save method of the Post model in the posts app. When a new post is saved, if the slug field is empty, it will be automatically generated using the title of the post. This ensures that each post has a unique and SEO-friendly URL.

--- socialproject/posts/models.py
+++ socialproject/posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 # Create your models here.
 
 
@@ -13,3 +14,8 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+            super().save(*args, **kwargs)
